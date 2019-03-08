895f9e662d4b16aab72094981b3fce28968a32bb
theonlyartz <gamesil456@gmail.com>
2019-03-08 18:08:08 +0300

    Player class initialize

---
+++ src/racer/player.cr
@@ -0,0 +1,15 @@
+class Player
+  @name:String
+  @avgWPM:Float
+  @wins:Int32
+
+  getter name
+  getter avgWPM
+  getter wins
+  
+  def initialize(properties: Hash(String, Int32 | String | Float64))
+      @name = properties["name"]
+      @avgWPM = properties["avgWPM"]
+      @wins = properties["wins"]
+  end
+end
