18d50b530a1392ee75b6ba2b4ce9daa493e76325
theonlyartz <gamesil456@gmail.com>
2019-03-09 18:24:40 +0300

    Directly passing the parameters

--- src/racer/player.cr
+++ src/racer/player.cr
@@ -1,9 +1,9 @@
 class Player
   getter id : String
-  getter avgWPM : Float64
+  getter avgWPM : Float64 | Int32
   getter wins : Int32
 
-  def initialize(@ws_manager : WS_manager, id : String, avgWPM : Float64, wins : Int32)
+  def initialize(@ws_manager : WS_manager, id : String, avgWPM : Float64 | Int32, wins : Int32)
       @id = id
       @avgWPM = avgWPM
       @wins = avgWPM
