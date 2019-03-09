6a4ea90d933facf3a930acfe059cb4a2e8b6dcfc
theonlyartz <gamesil456@gmail.com>
2019-03-09 17:44:32 +0300

    Not being redundant

--- src/racer/player.cr
+++ src/racer/player.cr
@@ -1,12 +1,8 @@
 class Player
-  @id : String
-  @avgWPM : Float64
-  @wins : Int32
-
-  getter id
-  getter avgWPM
-  getter wins
-
+  getter id : String
+  getter avgWPM : Float64
+  getter wins : Int32
+  
   def initialize(@ws_manager : WS_manager, properties : Hash(String, Int32 | String | Float64))
       @id = properties["id"]
       @avgWPM = properties["avgWPM"]
