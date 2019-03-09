53a1fb2d7b7deb25230c5d5e03984d3852b2ed28
theonlyartz <gamesil456@gmail.com>
2019-03-09 19:05:31 +0300

    Binding methods to class

--- src/racer/router_helper.cr
+++ src/racer/router_helper.cr
@@ -1,9 +1,9 @@
 class Router::Helper
-  def generate_user_id
+  def self.generate_user_id
     Random::Secure.urlsafe_base64(8, padding: true)
   end
 
-  def construct_player_payload(ws : WS_manager, id : String)
+  def self.construct_player_payload(ws : WS_manager, id : String)
     Player.new ws, id, 0, 0
   end
 end
