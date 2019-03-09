7e3a2729acd2b008154e13498d381e44747eede5
theonlyartz <gamesil456@gmail.com>
2019-03-09 18:22:31 +0300

    Directly passing the values

--- src/racer/player.cr
+++ src/racer/player.cr
@@ -2,10 +2,10 @@ class Player
   getter id : String
   getter avgWPM : Float64
   getter wins : Int32
-  
-  def initialize(@ws_manager : WS_manager, properties : Hash(String, Int32 | String | Float64))
-      @id = properties["id"]
-      @avgWPM = properties["avgWPM"]
-      @wins = properties["wins"]
+
+  def initialize(@ws_manager : WS_manager, id : String, avgWPM : Float64, wins : Int32)
+      @id = id
+      @avgWPM = avgWPM
+      @wins = avgWPM
   end
 end
