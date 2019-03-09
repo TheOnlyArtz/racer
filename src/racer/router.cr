4d6404bfdb915303078581f9cebb82072a486379
theonlyartz <gamesil456@gmail.com>
2019-03-09 17:50:29 +0300

    New router class

--- src/racer/router.cr
+++ src/racer/router.cr
@@ -1,4 +1,25 @@
-# A file to host all of the routes
-get "/" do |ctx|
-  render "src/views/index.ecr"
+# A file to host all of the route# A file to host all of the routes
+
+class Router
+  def initialize(@ws_manager : WS_manager)
+  end
+
+  # GET Routes
+  def set_gets
+    get "/" do |ctx|
+      render "src/views/index.ecr"
+    end
+  end
+
+  # POST routes
+  def set_post
+    post "/create_room" do |ctx|
+      if body = ctx.request.body
+         json = Hash(String, String).from_json body
+         if json.has_key?("owner_id")
+           room = Room.new
+         end
+      end
+    end
+  end
 end
