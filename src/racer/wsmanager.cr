7288c598d4d289eb9b9ef24259265f629edef648
theonlyartz <gamesil456@gmail.com>
2019-03-09 16:49:54 +0300

    wrote a macro to avoide boilerplating

--- src/racer/wsmanager.cr
+++ src/racer/wsmanager.cr
@@ -1,8 +1,23 @@
 # This manager will handle all of Racer's websocket
 # traffic including rooms and players
-class ws_manager
+
+macro assign(name, key, hash, value)
+  def {{name}}({{key}}, {{value}})
+    @{{hash}}[{{key}}] = {{value}}
+  end
+end
+
+class WS_manager
+  getter rooms
+  getter global_players
+
   def initialize()
     @rooms = {} of String => Player
     @global_players = {} of String => Player
   end
+
+  assign(assign_room, key, rooms, value)
+
+  assign(assign_player, key, global_players, value)
+
 end
