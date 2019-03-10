11f7c747d803c40645d9ffad9fb13d3b73d1ec61
theonlyartz <gamesil456@gmail.com>
2019-03-10 08:20:40 +0300

    Added methods to create a room and establish a ws connection accordingly

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,23 @@
-websocket_connection = 0;
-is_registered = false;
+let websocket_connection = 0;
+let is_registered = false;
+let curr_id = 0;
 
 function register_player() {
-   
+
+}
+
+async function create_room() {
+  try {
+    res = await axios.post("/create_room");
+    curr_id = res.data.room_id;
+    location.href = `/room/${curr_id}`
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 function connect_to_ws() {
-  websocket_connection = new WebSocket(`ws://${location.host}:3000/play`);
+  let a = location.href.split("http://localhost:3000/room/");
+
+  websocket_connection = new WebSocket(`ws://${location.host}/room/${a[1]}`);
 }
