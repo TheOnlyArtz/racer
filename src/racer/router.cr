c90398aba393cc533f1e4538fab8b5276d31a0f7
theonlyartz <gamesil456@gmail.com>
2019-03-10 08:21:14 +0300

    Routes

--- src/racer/router.cr
+++ src/racer/router.cr
@@ -9,23 +9,50 @@ class Router
     get "/" do |ctx|
       render "src/views/index.ecr"
     end
+
+    get "/room/:id" do |ctx|
+      room_id = ctx.params.url["id"]
+      if @ws_manager.rooms.has_key?(room_id)
+        render "src/views/room.ecr"
+      else
+        ctx.response.status_code = 404
+      end
+    end
   end
 
   # POST routes
   def set_posts
     post "/create_room" do |ctx|
-      if body = ctx.request.body
-         json = Hash(String, String).from_json body
-         if json.has_key?("owner_id")
-           
-           # Essentially this payload would exist inside the request body
-           # This is for the sake of testing
-           players_playload = {
-             json["owner_id"] => Player.new(self.@ws_manager, json["owner_id"], 0, 0)
-           }
+     new_id = Router::Helper.generate_user_id
+     room = Room.new(@ws_manager, new_id)
+     @ws_manager.assign_room new_id, room
 
-           room = Room.new(self.@ws_manager, players_playload)
-         end
+     ctx.response.print "{\"room_id\": \"#{new_id}\"}"
+    end
+
+    post "/register_player" do |ctx|
+      new_id = Router::Helper.generate_user_id
+      player = Router::Helper.construct_player_payload @ws_manager, new_id
+      @ws_manager.assign_player new_id, player
+    end
+  end
+
+  def set_sockets
+    ws "/room/:id" do |socket, ctx|
+      room_id = ctx.params.url["id"]
+      puts "socket coming in"
+      if @ws_manager.rooms.has_key?(room_id)
+        @ws_manager.@connections << socket
+      else
+        socket.close
+      end
+
+      socket.on_close do
+        @ws_manager.@connections.delete socket
+
+        if @ws_manager.@connections.size.zero?
+          @ws_manager.@rooms.delete room_id
+        end
       end
     end
   end
